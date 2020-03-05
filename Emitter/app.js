f36741628065a236fd2e2ba218e9dd57b0fe39e7
VuVietDuc <vuvietduc9x@gmail.com>
2020-03-05 18:59:13 +0700

    emitter

---
+++ Emitter/app.js
@@ -0,0 +1,20 @@
+var Emitter = require('./emitter');
+
+var emitter = new Emitter();
+
+emitter.on("bad", function() {
+    console.log("Một môn nào đó bị điểm kém !!!");
+});
+
+emitter.on("bad", function () {
+    console.log("Đã có điểm kém, cần phải thông báo tới phụ huynh");
+})
+
+var scores = [10, 4];
+
+for(var s of scores) {
+    if(s < 5) {
+        console.log("Điểm thấp quá", s);
+        emitter.emit("bad");
+    }
+}
