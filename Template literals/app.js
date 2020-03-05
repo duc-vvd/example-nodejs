b41e403766ef944dce0ee18cff6c555a5408237b
VuVietDuc <vuvietduc9x@gmail.com>
2020-03-05 20:00:33 +0700

    Template literals

---
+++ Template literals/app.js
@@ -0,0 +1,13 @@
+var person = {
+    name: "Mai Hoa",
+    age: 18
+}
+
+console.log("Tên tôi là " + person.name + " và tôi " + person.age + " tuổi !");
+console.log(`Tên tôi là ${person.name} và tôi ${person.age} tuổi !
+
+    Xuống được rất nhiều dòng
+    Còn có thể viết được dấu " ngoặc kép "
+
+
+`)
