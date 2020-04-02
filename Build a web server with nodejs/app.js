660fe3b126a296fd2e500886571bd48466b93774
VuVietDuc <vuvietduc9x@gmail.com>
2020-04-02 23:28:43 +0700

    Build a Web with Nodejs

---
+++ Build a web server with nodejs/app.js
@@ -0,0 +1,8 @@
+let http = require("http");
+
+http.createServer(function(req, res) {
+
+    res.writeHead(200, {'Content-type': 'text/plain'});
+    res.end("Hello Web from Node.js !");
+
+}).listen(1337, "127.0.0.1");
